74be323b075a0fb146c4d556d29e09fa0d621ad3
Gabriel Marcelo Santos Elizondo <52707386+Gsantos4@users.noreply.github.com>
2021-06-23 14:19:59 -0700

    Delete gathering_2 directory

--- gathering_2/Initializing_pins.ino
+++
@@ -1,9 +0,0 @@
-void setup() {  
-  // initialize digital pin 'x' as an output. x represents any pin that can output power on your board.  
-  pinMode(x, OUTPUT);  
-}  
-  
-// the loop function runs over and over again forever  
-void loop() {  
-  digitalWrite(x, HIGH);   // send power to 'x' pin (HIGH is the voltage level)  
-} 
